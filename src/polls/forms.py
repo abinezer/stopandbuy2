f9e60b85d042a96397c7ae412c1e04f9840d0cfe
abinezer <abishaim@outlook.com>
2020-07-18 13:28:13 +0530

    slight changes to ebay form

--- src/polls/forms.py
+++ src/polls/forms.py
@@ -1,4 +1,9 @@
 from django import forms
 
 class ebaySearchForm(forms.Form):
-    post = forms.CharField()
+    post = forms.CharField(widget = forms.TextInput(
+          attrs = {
+          'class': 'form-control',
+          'placeholder': 'Search ebay',
+          }
+    ))
